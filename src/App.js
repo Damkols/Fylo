70fa57ddb16239454db967a17b0c6029cfac48ba
damkols <damolausmankolapo@gmail.com>
2022-02-07 11:02:12 -0800

    fetch request to APi is good

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
-import "./App.css";
+import "./App.scss";
+import { fetchWeather } from "./Api/fetchWeather";
+import { useState } from "react";
 
 function App() {
+  const [query, setQuery] = useState("");
+
+  const search = async (e) => {
+    if (e.key === "Enter") {
+      const data = await fetchWeather(query);
+      console.log(data);
+    }
+  };
+
   return (
-    <div className="App">
-      <h1>App</h1>
+    <div className="container">
+      <input
+        type="text"
+        className="search"
+        placeholder="Search..."
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+        onKeyPress={search}
+      />
     </div>
   );
 }
